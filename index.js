20553196f186eb90f9939cb27e3113d957957d0b
barkley-wq <igonyibarkley00@gmail.com>
2024-07-05 12:06:12 +0300

    index.js

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength) {
+  if (rideLength <= 400) {
+    return 'This one is on me!';
+  } else if (rideLength > 400 && rideLength <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (rideLength > 2000 && rideLength <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
+}
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+      return "Thank you so much.";
+  } else if (tip === "not as generous") {
+      return "Thank you.";
+  } else {
+      return "Bye.";
+  }
 }
